da0e40b3d2b52dee15624c502f5ef58c952c07e2
Łukasz Wieczorek <wieczorek1990@gmail.com>
2014-11-02 13:16:17 +0100

    fixed bug, added last columns centering, added open graph meta

--- js/app.js
+++ js/app.js
@@ -23,8 +23,19 @@ var apps = $.getJSON('./js/apps.json', function (apps) {
         column[appName] = apps[appName];
         if (i % 3 == 2) {
             rows.push(column);
+            column = null;
         }
         i += 1;
     }
+    if (column != null) {
+        rows.push(column);
+    }
     $('#apps').html(Handlebars.templates.app({rows: rows}));
+    lastCols = $('.row:last > .col-lg-4');
+    lastColsCount = lastCols.length;
+    if (lastColsCount != 3) {
+        offset = lastColsCount == 1 ? 4 : 2;
+        className = 'col-lg-offset-' + offset;
+        lastCols.first().addClass(className);
+    }
 });
