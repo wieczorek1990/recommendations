506249c3badc2bdbf16812c04a9ce71805fcf5c4
Łukasz Wieczorek <luke.wieczorek@outlook.com>
2015-07-21 16:49:21 +0200

    Updated templates.

--- js/item.compiled.js
+++ js/item.compiled.js
@@ -1,51 +1,50 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['item'] = template({"1":function(depth0,helpers,partials,data,depths) {
-  var stack1, buffer = "    <div class=\"row\">\n";
-  stack1 = helpers.each.call(depth0, depth0, {"name":"each","hash":{},"fn":this.program(2, data, depths),"inverse":this.noop,"data":data});
-  if (stack1 != null) { buffer += stack1; }
-  return buffer + "    </div>\n";
-},"2":function(depth0,helpers,partials,data,depths) {
-  var stack1, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda, buffer = "            <div class="
-    + escapeExpression(((helpers.col || (depth0 && depth0.col) || helperMissing).call(depth0, (depths[2] != null ? depths[2].colSize : depths[2]), {"name":"col","hash":{},"data":data})))
+templates['item'] = template({"1":function(depth0,helpers,partials,data,blockParams,depths) {
+    var stack1;
+
+  return "    <div class=\"row\">\n"
+    + ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(2, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "    </div>\n";
+},"2":function(depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, alias1=helpers.helperMissing, alias2=this.escapeExpression;
+
+  return "            <div class="
+    + alias2((helpers.col || (depth0 && depth0.col) || alias1).call(depth0,(depths[2] != null ? depths[2].colSize : depths[2]),{"name":"col","hash":{},"data":data}))
     + ">\n                <h2 class=\"text-center\">"
-    + escapeExpression(lambda((depth0 != null ? depth0.title : depth0), depth0))
-    + "</h2>\n\n";
-  stack1 = ((helpers.ifImage || (depth0 && depth0.ifImage) || helperMissing).call(depth0, (depth0 != null ? depth0.image : depth0), (depths[2] != null ? depths[2].platform : depths[2]), {"name":"ifImage","hash":{},"fn":this.program(3, data, depths),"inverse":this.noop,"data":data}));
-  if (stack1 != null) { buffer += stack1; }
-  buffer += "\n";
-  stack1 = ((helpers.ifShort || (depth0 && depth0.ifShort) || helperMissing).call(depth0, (depth0 != null ? depth0.description : depth0), {"name":"ifShort","hash":{},"fn":this.program(5, data, depths),"inverse":this.program(7, data, depths),"data":data}));
-  if (stack1 != null) { buffer += stack1; }
-  return buffer + "\n                <p class=\"text-center\">\n                    <a class=\"btn btn-primary\" href="
-    + escapeExpression(((helpers.link || (depth0 && depth0.link) || helperMissing).call(depth0, (data && data.key), (depths[2] != null ? depths[2].platform : depths[2]), depth0, {"name":"link","hash":{},"data":data})))
+    + alias2(this.lambda((depth0 != null ? depth0.title : depth0), depth0))
+    + "</h2>\n\n"
+    + ((stack1 = (helpers.ifImage || (depth0 && depth0.ifImage) || alias1).call(depth0,(depth0 != null ? depth0.image : depth0),(depths[2] != null ? depths[2].platform : depths[2]),{"name":"ifImage","hash":{},"fn":this.program(3, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "\n"
+    + ((stack1 = (helpers.ifShort || (depth0 && depth0.ifShort) || alias1).call(depth0,(depth0 != null ? depth0.description : depth0),{"name":"ifShort","hash":{},"fn":this.program(5, data, 0, blockParams, depths),"inverse":this.program(7, data, 0, blockParams, depths),"data":data})) != null ? stack1 : "")
+    + "\n                <p class=\"text-center\">\n                    <a class=\"btn btn-primary\" href="
+    + alias2((helpers.link || (depth0 && depth0.link) || alias1).call(depth0,(data && data.key),(depths[2] != null ? depths[2].platform : depths[2]),depth0,{"name":"link","hash":{},"data":data}))
     + " role=\"button\">\n                        "
-    + escapeExpression(((helpers.go || (depth0 && depth0.go) || helperMissing).call(depth0, (depths[2] != null ? depths[2].platform : depths[2]), {"name":"go","hash":{},"data":data})))
+    + alias2((helpers.go || (depth0 && depth0.go) || alias1).call(depth0,(depths[2] != null ? depths[2].platform : depths[2]),{"name":"go","hash":{},"data":data}))
     + "\n                    </a>\n                </p>\n            </div>\n";
-},"3":function(depth0,helpers,partials,data,depths) {
-  var helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda;
+},"3":function(depth0,helpers,partials,data,blockParams,depths) {
+    var alias1=helpers.helperMissing, alias2=this.escapeExpression;
+
   return "                    <a href=\""
-    + escapeExpression(((helpers.link || (depth0 && depth0.link) || helperMissing).call(depth0, (data && data.key), (depths[3] != null ? depths[3].platform : depths[3]), depth0, {"name":"link","hash":{},"data":data})))
+    + alias2((helpers.link || (depth0 && depth0.link) || alias1).call(depth0,(data && data.key),(depths[3] != null ? depths[3].platform : depths[3]),depth0,{"name":"link","hash":{},"data":data}))
     + "\">\n                        <img class=\"img-responsive center-block\"\n                             style=\"width: "
-    + escapeExpression(((helpers.size || (depth0 && depth0.size) || helperMissing).call(depth0, (depths[3] != null ? depths[3].platform : depths[3]), {"name":"size","hash":{},"data":data})))
+    + alias2((helpers.size || (depth0 && depth0.size) || alias1).call(depth0,(depths[3] != null ? depths[3].platform : depths[3]),{"name":"size","hash":{},"data":data}))
     + "px; height: auto;\"\n                             src="
-    + escapeExpression(((helpers.img || (depth0 && depth0.img) || helperMissing).call(depth0, (data && data.key), (depths[3] != null ? depths[3].platform : depths[3]), {"name":"img","hash":{},"data":data})))
+    + alias2((helpers.img || (depth0 && depth0.img) || alias1).call(depth0,(data && data.key),(depths[3] != null ? depths[3].platform : depths[3]),{"name":"img","hash":{},"data":data}))
     + " alt="
-    + escapeExpression(lambda((depth0 != null ? depth0.title : depth0), depth0))
+    + alias2(this.lambda((depth0 != null ? depth0.title : depth0), depth0))
     + ">\n                    </a>\n";
 },"5":function(depth0,helpers,partials,data) {
-  var lambda=this.lambda, escapeExpression=this.escapeExpression;
-  return "                    <p class=\"lead text-center\">"
-    + escapeExpression(lambda((depth0 != null ? depth0.description : depth0), depth0))
+    return "                    <p class=\"lead text-center\">"
+    + this.escapeExpression(this.lambda((depth0 != null ? depth0.description : depth0), depth0))
     + "</p>\n";
 },"7":function(depth0,helpers,partials,data) {
-  var lambda=this.lambda, escapeExpression=this.escapeExpression;
-  return "                    <p class=\"lead text-justify\">"
-    + escapeExpression(lambda((depth0 != null ? depth0.description : depth0), depth0))
+    return "                    <p class=\"lead text-justify\">"
+    + this.escapeExpression(this.lambda((depth0 != null ? depth0.description : depth0), depth0))
     + "</p>\n";
-},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data,depths) {
-  var stack1, buffer = "";
-  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.rows : depth0), {"name":"each","hash":{},"fn":this.program(1, data, depths),"inverse":this.noop,"data":data});
-  if (stack1 != null) { buffer += stack1; }
-  return buffer;
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data,blockParams,depths) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.rows : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true,"useDepths":true});
 })();
--- js/nav.compiled.js
+++ js/nav.compiled.js
@@ -1,22 +1,23 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['nav'] = template({"1":function(depth0,helpers,partials,data) {
-  var stack1, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda, buffer = "";
-  stack1 = helpers['if'].call(depth0, (depth0 != null ? depth0.isActive : depth0), {"name":"if","hash":{},"fn":this.program(2, data),"inverse":this.program(4, data),"data":data});
-  if (stack1 != null) { buffer += stack1; }
-  return buffer + "            <a href="
-    + escapeExpression(((helpers.href || (depth0 && depth0.href) || helperMissing).call(depth0, (data && data.key), {"name":"href","hash":{},"data":data})))
+    var stack1, alias1=this.escapeExpression;
+
+  return ((stack1 = helpers['if'].call(depth0,(depth0 != null ? depth0.isActive : depth0),{"name":"if","hash":{},"fn":this.program(2, data, 0),"inverse":this.program(4, data, 0),"data":data})) != null ? stack1 : "")
+    + "            <a href="
+    + alias1((helpers.href || (depth0 && depth0.href) || helpers.helperMissing).call(depth0,(data && data.key),{"name":"href","hash":{},"data":data}))
     + ">"
-    + escapeExpression(lambda((depth0 != null ? depth0.title : depth0), depth0))
+    + alias1(this.lambda((depth0 != null ? depth0.title : depth0), depth0))
     + "</a>\n        </li>\n";
 },"2":function(depth0,helpers,partials,data) {
-  return "            <li class=\"active\">\n";
-  },"4":function(depth0,helpers,partials,data) {
-  return "            <li>\n";
-  },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1, buffer = "<ul class=\"nav nav-justified\">\n";
-  stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.links : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
-  if (stack1 != null) { buffer += stack1; }
-  return buffer + "</ul>\n";
+    return "            <li class=\"active\">\n";
+},"4":function(depth0,helpers,partials,data) {
+    return "            <li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<ul class=\"nav nav-justified\">\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.links : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "</ul>\n";
 },"useData":true});
 })();
